a55c1dfbfe3210e7b679336385f75bedae5cb4b0
Maurice Fiorani <mauricef97@gmail.com>
2020-02-27 04:27:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,12 @@ require 'pry'
 def create_project_hash
  html = File.read('fixtures/kickstarter.html')
  kickstarter = Nokogiri::HTML(html)
- binding.pry
+ projects = {}
+ 
+ 
 end
 
-create_project_hash
+
 
 
 # projects: kickstarter.css("li.project.grid_4")
